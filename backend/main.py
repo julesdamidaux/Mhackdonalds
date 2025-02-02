9ed7e2f4d0e1e6cc8a97bb1f3c9911419df5ecfb
crabedesneiges <maxencealbert2002@gmail.com>
2025-02-02 08:46:34 +0100

    Refracting Code

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,2 @@
-from create_database_json_from_database import get_database_json_from_database
-
-db_path = "data/veolia_data.db"
-contexte = "test réussi"
-input_json = "data/input.json"
-
 if __name__ == "__main__":
-
-    database_json = get_database_json_from_database(db_path, contexte, input_json, save_json=False)
-
-
-"""
-
-subprocess.run(["python", "generate_constraints.py"], capture_output=True, text=True)"""
+    print("Hello World")
