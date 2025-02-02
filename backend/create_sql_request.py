3a0f65e13ddbffe81c8629c7de588229c3cb7ca6
crabedesneiges <maxencealbert2002@gmail.com>
2025-02-02 09:21:50 +0100

    Update create_sql_request.py

--- backend/create_sql_request.py
+++ backend/create_sql_request.py
@@ -6,6 +6,13 @@ import sys
 
 from credentials import region_name, aws_access_key_id, aws_secret_access_key
 
+with open("credentials.json", "r", encoding="utf-8") as f:
+    config = json.load(f)
+
+    aws_access_key_id = config.get("aws_access_key_id")
+    aws_secret_access_key = config.get("aws_secret_access_key")
+    region_name = config.get("region_name")
+
 def create_sql_request(MODEL_ID,bedrock,input_data,db_json):
     
     def prompt(constraint):
